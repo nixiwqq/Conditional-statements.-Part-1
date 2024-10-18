862eddd91524aa64b12b77bbac3a0656ea3dbec9
Nika <129578493+nixiwqq@users.noreply.github.com>
2024-10-18 20:24:40 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+#number =  int(input('Enter a number:'))
+#if number % 2 == 0:
+#    print(number, 'it\'s Even number')
+#else:
+#    print(number, 'it\'s Odd number')
+
+
+
+#task 2
+#number =  int(input('Enter a number:'))
+#if number % 7 ==0:
+#    print(number, 'is a multiple of 7')
+#else:
+#    print(number, 'is not a multiple of 7')
+
+
+#task 3
+#number =  int(input('Enter a number:'))
+#another_number =  int(input('Enter another number:'))
+#if number >= another_number:
+#    print(number)
+#else:
+#    print(another_number)
+
+
+#task 4
+number =  int(input('Enter a number:'))
+another_number =  int(input('Enter another number:'))
+if number <= another_number:
+    print(number)
+else:
+    print(another_number)
